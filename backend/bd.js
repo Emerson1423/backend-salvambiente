3074bf9ec9c98a860e2cc6f372d342bd4a68df2f
Emerson1423 <emersonaldahir2453@gmail.com>
2025-11-24 23:38:07 -0600

    1

--- backend/bd.js
+++ backend/bd.js
@@ -3,10 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost', // Ej: 'localhost'
-  user: process.env.DB_USER || 'root',     // Ej: 'root'
-  password: process.env.DB_PASSWORD || '', // Reemplaza con tu contraseña
-  database: process.env.DB_NAME || 'huella_de_carbono',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10
 });
